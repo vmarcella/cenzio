b79dfd2a1c62797357b7ad174160ffe04ff3b2cc
C3NZ <cenz@cenz.io>
2018-12-26 01:39:43 -0800

    [feat] update variable declarations and document code

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// node module and std lib imports
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Import our routers
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+// create our server
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Setup middleware that needs to be hit before our routes
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Setup our routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
